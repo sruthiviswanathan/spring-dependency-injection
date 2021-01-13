6ca1992982843aa4cb135d9222545d69971e6b36
sruthiviswanathan <sravan1097@gmail.com>
2021-01-13 12:09:50 +0530

    Setting default profile

--- src/main/java/com/example/springdi/services/i18nEnglishGreetingService.java
+++ src/main/java/com/example/springdi/services/i18nEnglishGreetingService.java
@@ -1,10 +1,10 @@
 package com.example.springdi.services;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class i18nEnglishGreetingService implements GreetingService {
 
